d416937a8daaa48b252589a15fd83e3233863bfd
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-07 09:00:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ function handleDOMLoad() {
+            // Write "DOM load success" when the DOM is loaded
+            console.log("DOM load success");
+        }
+
+        document.addEventListener("DOMContentLoaded", handleDOMLoad);
