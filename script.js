6007255dd8727128bcd9c581a584d2c774531394
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-07 09:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //your JS code here. If required.
- function handleDOMLoad() {
-            // Write "DOM load success" when the DOM is loaded
-            console.log("DOM load success");
+  function handleDOMLoad() {
+            // Create a new paragraph element
+            var paragraph = document.createElement("p");
+            // Set the text content to "DOM load success"
+            paragraph.textContent = "DOM load success";
+            
+            // Append the paragraph to the body element
+            document.body.appendChild(paragraph);
         }
 
         document.addEventListener("DOMContentLoaded", handleDOMLoad);
